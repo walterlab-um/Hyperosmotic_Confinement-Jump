5f929fb73adacedce56144ddbb445a5b747d8b66
Bisal Halder (He/Him/His) <157258694+bisalumich@users.noreply.github.com>
2025-01-29 15:08:30 -0500

    Update split_same_channel_ALEX_with subtraction.py

--- Camera_Registration/split_same_channel_ALEX_with subtraction.py
+++ Camera_Registration/split_same_channel_ALEX_with subtraction.py
@@ -21,19 +21,22 @@ for fname in track(lst_files):
     video_odd = np.delete(deepcopy(video), frames_even, 0)
     video_even = np.delete(deepcopy(video), frames_odd, 0)
 
-    video_odd_modified = video_odd - 0.95 * (video_even)
-    video_odd_modified = video_odd_modified.astype(np.float32)
+    # video_odd_modified = video_odd - 0.95 * (video_even)
+    # video_odd_modified = video_odd_modified.astype(np.float32)
+
+    video_even_modified = video_even - 0.4 * (video_odd)
+    video_even_modified = video_even_modified.astype(np.float32)
 
     imwrite(
-        fname.strip(".tif") + "-odd_modified.tif",
-        video_odd_modified,
+        fname.strip(".tif") + "-odd.tif",
+        video_odd,
         imagej=True,
         metadata={"axes": "TYX"},
     )
 
     imwrite(
-        fname.strip(".tif") + "-even.tif",
-        video_even,
+        fname.strip(".tif") + "-even_modified.tif",
+        video_even_modified,
         imagej=True,
         metadata={"axes": "TYX"},
     )
